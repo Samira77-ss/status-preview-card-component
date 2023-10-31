6a0122b852b576b4ed176c0f4fd563e35794e8b1
Samira77-ss <samiraseyfii0709@gmail.com>
2023-10-31 12:52:43 +0330

    Added footer

--- src/Card.js
+++ src/Card.js
@@ -2,35 +2,57 @@ import "./Card.css";
 
 function Card() {
   return (
-    <div className="card">
-      <div className="image"></div>
-      <div className="content">
-        <div className="info">
-          <h1>
-            Get <spn className="insight">insights</spn> that help your business
-            grow.
-          </h1>
-          <p>
-            {" "}
-            Discover the benefits of data analytics and make better decisions
-            regarding revenue, customer experience, and overall efficiency.{" "}
-          </p>
-        </div>
-        <div className="features">
-          <div className="feature">
-            <span className="number">10k+</span>
-            <span className="further">COMPANIES</span>
+    <div className="container">
+      <div className="card">
+        <div className="image"></div>
+        <div className="content">
+          <div className="info">
+            <h1>
+              Get <spn className="insight">insights</spn> that help your
+              business grow.
+            </h1>
+            <p>
+              {" "}
+              Discover the benefits of data analytics and make better decisions
+              regarding revenue, customer experience, and overall efficiency.{" "}
+            </p>
           </div>
-          <div className="feature">
-            <span className="number">314</span>
-            <span className="further">TEMPLATES</span>
-          </div>
-          <div className="feature">
-            <span className="number">12M+</span>
-            <span className="further">QUERIES</span>
+          <div className="features">
+            <div className="feature">
+              <span className="number">10k+</span>
+              <span className="further">COMPANIES</span>
+            </div>
+            <div className="feature">
+              <span className="number">314</span>
+              <span className="further">TEMPLATES</span>
+            </div>
+            <div className="feature">
+              <span className="number">12M+</span>
+              <span className="further">QUERIES</span>
+            </div>
           </div>
         </div>
       </div>
+      <footer>
+        This page was coded by Samira Seyfi
+        <a
+          href="https://github.com/Samira77-ss/challenge-order"
+          target="_blank"
+          alt="GithHub Profile"
+          rel="noreferrer"
+        >
+          open-source-code on GitHub
+        </a>
+        , and hosted on
+        <a
+          href="https://resonant-medovik-e01702.netlify.app"
+          alt="Netlify link"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }
